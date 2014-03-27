baa01bc0249424dbe4f2417550b3aabcf589180e
Patrick Jones <pmichaeljones@gmail.com>
2014-03-27 15:22:57 -0600

    Starting the ex47 project

---
+++ test/test_ex47.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+class MyUnitTest < Test::Unit::TestCase
+
+	def setup
+		puts "setup!"
+	end
+
+	def teardown
+		puts "teardown!"
+	end
+
+	def test_basic
+		puts "I ran!"
+	end
+end
